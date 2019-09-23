091a824a8b176e8eb8b910e18c9c5fdb01a850ca
odykyi <odykyi@alteryx.com>
2019-09-23 16:56:33 +0300

    isArmstrongNumber

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -3,9 +3,6 @@ export default {
     return arr.reduce((acc: number, curr: number) => acc + curr**arr.length, 0);
   },
   isArmstrongNumber(num: number): boolean {
-    const str: string = String(num);
-    const arr = str.split('').map(Number);
-
-    return num === this.sum(arr);
+    return num === this.sum(String(num).split('').map(Number));
   }
 }
