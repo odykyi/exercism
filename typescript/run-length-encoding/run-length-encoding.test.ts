25e1342b8deb31dc531bf8e2383750d374a4f54f
odykyi <odykyi@alteryx.com>
2019-10-10 18:41:10 +0300

    ts RunLengthEncoding

--- typescript/run-length-encoding/run-length-encoding.test.ts
+++ typescript/run-length-encoding/run-length-encoding.test.ts
@@ -35,38 +35,38 @@ describe("run-length encode a string", () => {
     expect(RunLengthEncoding.encode("aabbbcccc")).toEqual(expected)
   })
 })
-//
-// describe("run-length decode a string", () => {
-//   it("empty string", () => {
-//     const expected = ""
-//     expect(RunLengthEncoding.decode("")).toEqual(expected)
-//   })
-//
-//   it("single characters only", () => {
-//     const expected = "XYZ"
-//     expect(RunLengthEncoding.decode("XYZ")).toEqual(expected)
-//   })
-//
-//   it("string with no single characters", () => {
-//     const expected = "AABBBCCCC"
-//     expect(RunLengthEncoding.decode("2A3B4C")).toEqual(expected)
-//   })
-//
-//   it("single characters with repeated characters", () => {
-//     const expected = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
-//     expect(RunLengthEncoding.decode("12WB12W3B24WB")).toEqual(expected)
-//   })
-//
-//   it("multiple whitespace mixed in string", () => {
-//     const expected = "  hsqq qww  "
-//     expect(RunLengthEncoding.decode("2 hs2q q2w2 ")).toEqual(expected)
-//   })
-//
-//   it("lower case string", () => {
-//     const expected = "aabbbcccc"
-//     expect(RunLengthEncoding.decode("2a3b4c")).toEqual(expected)
-//   })
-// })
+
+describe("run-length decode a string", () => {
+  it("empty string", () => {
+    const expected = ""
+    expect(RunLengthEncoding.decode("")).toEqual(expected)
+  })
+
+  it("single characters only", () => {
+    const expected = "XYZ"
+    expect(RunLengthEncoding.decode("XYZ")).toEqual(expected)
+  })
+
+  it("string with no single characters", () => {
+    const expected = "AABBBCCCC"
+    expect(RunLengthEncoding.decode("2A3B4C")).toEqual(expected)
+  })
+
+  it("single characters with repeated characters", () => {
+    const expected = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
+    expect(RunLengthEncoding.decode("12WB12W3B24WB")).toEqual(expected)
+  })
+
+  it("multiple whitespace mixed in string", () => {
+    const expected = "  hsqq qww  "
+    expect(RunLengthEncoding.decode("2 hs2q q2w2 ")).toEqual(expected)
+  })
+
+  it("lower case string", () => {
+    const expected = "aabbbcccc"
+    expect(RunLengthEncoding.decode("2a3b4c")).toEqual(expected)
+  })
+})
 //
 // describe("encode and then decode", () => {
 //   it("encode followed by decode gives original string", () => {
--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -1,36 +1,127 @@
 export default {
-  objectToString(obj: any) {
-    const arr: any[] = [];
-    Object.values(obj).forEach((el, index) => {
-      if (el != 1) arr.push(el);
-      arr.push(Object.keys(obj)[index]);
-    });
-    return arr.join('');
-  },
-  encode(str: string): string {
-    if (!str) return '';
-    const arr = str.split('');
-    let stack: any[] = [];
-    const obj: any = {};
-    arr.forEach((word, index) => {
-      if (stack[index] === word || stack[index - 1] === word) {
-        obj[word] = obj[word] ? obj[word] + 1 : 2;
-        stack = [];
-      } else {
-        obj[word] = obj[word] ? obj[word] + 1 : 1;
-        stack[index] = word;
+  encode(code: string): string {
+    if (!code) return '';
+    let encode = '';
+
+    for (let i = 0; i < code.length; i++) {
+      let count = 1;
+      for (let j = i; j < code.length; j++) {
+        if (code[i] !== code[j+1]) break;
+        count++;
+        i++;
       }
-    });
+      encode += count === 1 ? code[i] : count + code[i];
+    }
 
-
-    console.log('obj', obj);
-    console.log('stack', stack);
-    return this.objectToString(obj);
+    return encode
   },
-  // decode(str: string): string {
-  //   return '';
-  // }
+  // "2A3B4C"
+  // "AABBBCCCC"
+  decode(code: string): string {
+    let decode = '';
+    console.log('code', code);
+
+    for (let i = 0; i < code.length; i++) {
+      let count = Number.isInteger(+code[i]) ? +code[i] : 1;
+      let char = !Number.isInteger(+code[i]) ? code[i] : '';
+      console.log('count', count);
+
+      for (let j = 0; j < count; j++) {
+        // console.log('----code[i]', code[i]);
+        // console.log('----code[j]', code[j]);
+        console.log('----char', char);
+
+        decode += char;
+      }
+    }
+    console.log('----');
+    console.log('decode', decode);
+    // decode =
+    return decode;
+  }
+
+//   encode(code: string): string {
+//     if (!code) return '';
+//     let encode = '';
+//
+//     for (let i = 0; i < code.length; i++) {
+//       let count = 1;
+//       for (let j = i; j < code.length; j++) {
+//         if (code[i] !== code[j+1]) break;
+//         count++;
+//         i++;
+//       }
+//       encode += count === 1 ? code[i] : count + code[i];
+//     }
+//
+//     return encode
+//   },
+// }
 }
+
+//
+// export default {
+//   encode(code: string): string {
+//     if (!code) return '';
+//     let encode = '';
+//
+//     for (let i = 0; i < code.length; i++){
+//       const smb = code[i] ;
+//       let count = 1;
+//       for (let j = i; j < code.length; j++){
+//         if (code[j + 1] != smb) break ;
+//         count++ ;
+//         i++ ;
+//       }
+//
+//       if (count === 1) {
+//         encode += smb ;
+//       } else {
+//         encode += count + smb ;
+//       }
+//     }
+//     return encode
+//   },
+// }
+
+
+//
+// export default {
+//   objectToString(obj: any) {
+//     const arr: any[] = [];
+//     Object.values(obj).forEach((el, index) => {
+//       if (el != 1) arr.push(el);
+//       arr.push(Object.keys(obj)[index]);
+//     });
+//     return arr.join('');
+//   },
+//   encode(str: string): string {
+//     if (!str) return '';
+//     const arr = str.split('');
+//     let stack: any[] = [];
+//     const obj: any = {};
+//     arr.forEach((word, index) => {
+//       if (stack[index] === word || stack[index - 1] === word) {
+//         if (arr[index + 1] !== word) {
+//           delete obj[word]
+//         }
+//         obj[word] = obj[word] ? obj[word] + 1 : 2;
+//         stack = [];
+//       } else {
+//         obj[word] = obj[word] ? obj[word] + 1 : 1;
+//         stack[index] = word;
+//       }
+//     });
+//
+//
+//     console.log('obj', obj);
+//     console.log('stack', stack);
+//     return this.objectToString(obj);
+//   },
+//   // decode(str: string): string {
+//   //   return '';
+//   // }
+// }
 
 // export default {
 //   encode(str: string): string {
