ce154c9085585b85b273db6530c6ed253f1929e7
odykyi <odykyi@alteryx.com>
2019-10-30 16:20:47 +0200

    tt

--- typescript/binary-search/binary-search.test.ts
+++ typescript/binary-search/binary-search.test.ts
@@ -4,8 +4,9 @@ describe('BinarySearch find()', () => {
   const binarySearch = new BinarySearch();
 
   it('binarySearch', () => {
-    const arr = new Array(10).fill(1).map((_, i) => i + 10);
+    // const arr = new Array(10).fill(1).map((_, i) => i + 10);
+    const arr = [3,11,21,29,41,54,61,78,110,127];
     console.log('arr', arr);
-    expect(binarySearch.find(arr, 13)).toBe(3);
+    expect(binarySearch.find(arr, 29)).toBe(3);
   })
 });
--- typescript/binary-search/binary-search.ts
+++ typescript/binary-search/binary-search.ts
@@ -2,37 +2,50 @@ export default class BinarySearch {
   find(array: number[], input: number): number | null {
     let res = null;
     let lim = array.length - 1;
+    let counter = 0;
+    let mid = Math.floor(lim / 2);
 
     for (let i = 0; i < lim;) {
-      const firstIndex = i;
-      const lastIndex = array.length - 1 - i;
-      const firstElement = array[firstIndex];
-      if (!firstElement) {
-        break
-      }
-      const lastElement = array[lastIndex];
-      console.log('------start---------');
-      console.log(`firstIndex ${firstIndex}, lastIndex ${lastIndex}, firstElement ${firstElement}, lastElement ${lastElement}`);
+      counter++
+      if (counter === 11) throw new Error('STOP!');
+      if (array[mid] > input) {
+        console.log('BIG then input');
+        lim = mid;
+        i = Math.floor(mid / 4);
+        mid = Math.floor(mid / 2);
 
-      const guessIndex = Math.floor((firstIndex + lastIndex) / 2);
-      console.log(`guessIndex ${guessIndex}, array[guessIndex] ${array[guessIndex]}, lim ${lim}, i ${i}`);
-
-      if (array[guessIndex] > input) {
-        console.log('big then input');
-        lim = guessIndex;
-        i++;
+        console.log('lim', lim);
+        console.log('i', i);
+        console.log('mid', mid);
+        continue
       }
-      else if (array[guessIndex] < input) {
-        console.log('+++++++++++');
-        console.log('smaller then input');
-        lim = lim / 2;
+      else if (array[mid] < input) {
+        console.log('SMALL then input');
+        console.log('lim', lim);
+        console.log('i', i);
+        console.log('mid', mid);
+        mid = Math.floor(mid * 2);
+        // lim = lim / 2;
+        continue
       } else {
         console.log('FIND!', i)
         res = i;
-        break;
+        continue;
       }
+
     }
 
     return res;
   }
 }
+
+// const firstElement = array[firstIndex];
+// if (!firstElement) {
+//   break
+// }
+// const lastElement = array[lastIndex];
+// console.log('------start---------');
+// console.log(`firstIndex ${firstIndex}, lastIndex ${lastIndex}, firstElement ${firstElement}, lastElement ${lastElement}`);
+//
+// const guessIndex = Math.floor((firstIndex + lastIndex) / 2);
+// console.log(`guessIndex ${guessIndex}, array[guessIndex] ${array[guessIndex]}, lim ${lim}, i ${i}`);
