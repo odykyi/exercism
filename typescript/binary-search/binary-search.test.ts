982247d7c3b802f66620c228cfee6aae4df6ce7f
odykyi <odykyi@alteryx.com>
2019-10-30 14:55:09 +0200

    tt

--- typescript/binary-search/binary-search.test.ts
+++ typescript/binary-search/binary-search.test.ts
@@ -1,12 +1,11 @@
 import BinarySearch from './binary-search'
-import ArmstrongNumbers from "../armstrong-numbers/armstrong-numbers";
 
 describe('BinarySearch find()', () => {
   const binarySearch = new BinarySearch();
 
   it('binarySearch', () => {
-    const arr = new Array(100).fill(1).map((_, i) => i + 1);
+    const arr = new Array(10).fill(1).map((_, i) => i + 10);
     console.log('arr', arr);
-    expect(binarySearch.find(arr, 45)).toBe(44);
+    expect(binarySearch.find(arr, 13)).toBe(3);
   })
 });
--- typescript/binary-search/binary-search.ts
+++ typescript/binary-search/binary-search.ts
@@ -1,20 +1,38 @@
 export default class BinarySearch {
   find(array: number[], input: number): number | null {
     let res = null;
-    for (let i = 0; i < array.length; i++){
-      if((array[i] + array[array.length - 1 + i]) / 2 > input) {
-        console.log('big')
+    let lim = array.length - 1;
+
+    for (let i = 0; i < lim;) {
+      const firstIndex = i;
+      const lastIndex = array.length - 1 - i;
+      const firstElement = array[firstIndex];
+      if (!firstElement) {
+        break
       }
-      else if((array[i] + array[array.length - 1 + i]) / 2 < input) {
-        console.log('small')
-      } else if (array[i] === input){
+      const lastElement = array[lastIndex];
+      console.log('------start---------');
+      console.log(`firstIndex ${firstIndex}, lastIndex ${lastIndex}, firstElement ${firstElement}, lastElement ${lastElement}`);
+
+      const guessIndex = Math.floor((firstIndex + lastIndex) / 2);
+      console.log(`guessIndex ${guessIndex}, array[guessIndex] ${array[guessIndex]}, lim ${lim}, i ${i}`);
+
+      if (array[guessIndex] > input) {
+        console.log('big then input');
+        lim = guessIndex;
+        i++;
+      }
+      else if (array[guessIndex] < input) {
+        console.log('+++++++++++');
+        console.log('smaller then input');
+        lim = lim / 2;
+      } else {
+        console.log('FIND!', i)
         res = i;
         break;
       }
-
-      console.log('iteration', i)
     }
-    return res;
 
+    return res;
   }
 }
